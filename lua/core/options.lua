b6b296f96c1cdb67f4fbb9c544b3b8aeda5dffd4
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-08-22 17:33:34 +0100

    chore: increase guifont size

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -47,7 +47,7 @@ o.backup = false
 o.swapfile = false
 
 -- Font
-o.guifont = "ZedMono Nerd Font:h10.5"
+o.guifont = "ZedMono Nerd Font:h15"
 -- Colorscheme
 vim.cmd("colorscheme kagayaki")
 -- Transparent nontext
